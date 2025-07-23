6281478816d19922f4deab0f4b8d497a4b36c35d
Gabriel Silva <devgabrielsj@gmail.com>
2025-07-23 00:15:47 -0300

    mudei para url no properties

--- src/main/java/com/br/hexa_api/adapter/JsonPlaceholderClient.java
+++ src/main/java/com/br/hexa_api/adapter/JsonPlaceholderClient.java
@@ -1,21 +1,14 @@
 package com.br.hexa_api.adapter;
 
-
-
 import com.br.hexa_api.domain.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 import java.util.List;
 
-
-@FeignClient(name = "jsonPlaceholder", url = "https://jsonplaceholder.typicode.com")
+@FeignClient(name = "jsonplaceholder", url = "${spring.cloud.openfeign.clients.config.jsonplaceholder.url}")
 public interface JsonPlaceholderClient {
-
     @GetMapping("/users")
     List<User> getUsers();
 }
-
-
 
